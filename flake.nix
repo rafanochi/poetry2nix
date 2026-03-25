dfcfc0e01997b5dc52a547f936968d0d04c53a07
rafanochi <zawkindev@gmail.com>
2026-03-26 00:45:40 +0500

    chore: change nixpkgs to unstable

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     # Last working commit from nixos-small-unstable
-    nixpkgs.url = "github:xinux-org/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:xinux-org/nixpkgs/nixos-unstable";
 
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
